227f4bcc1e70ac3193e994830a79031e97beaf68
Ipan Taupik Rahman <ipantaupik97@gmail.com>
2020-09-23 14:25:20 +0700

    branch-sample

---
+++ go.mod
@@ -0,0 +1,9 @@
+module branca-sample
+
+go 1.15
+
+require (
+	github.com/hako/branca v0.0.0-20200807062402-6052ac720505
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
+	golang.org/x/sys v0.0.0-20200922070232-aee5d888a860 // indirect
+)
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hako/branca"
+)
+
+func main() {
+	b := branca.NewBranca("supersecretkeyyoushouldnotcommit") // This key must be exactly 32 bytes long.
+
+	// Encode String to Branca Token.
+	token, err := b.EncodeToString(`{"name":"KPM","signature_url":"https://"}`)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// b.SetTTL(3600) // Uncomment this to set an expiration (or ttl) of the token (in seconds).
+	//token = "87y8daMzSkn7PA7JsvrTT0JUq1OhCjw9K8w2eyY99DKru9FrVKMfeXWW8yB42C7u0I6jNhOdL5ZqL" // This token will be not allowed if a ttl is set.
+
+	// Decode Branca Token.
+	message, err := b.DecodeToString(token)
+	if err != nil {
+		fmt.Println(err) // token is expired.
+		return
+	}
+	fmt.Println(token)   // 87y8da....
+	fmt.Println(message) // Hello world!
+}
